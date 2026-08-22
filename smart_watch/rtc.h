b2a2c5f86009dd4e91ff65b962f5d1d9e1fbe65c
Sooraj D S <soorajjds@gmail.com>
2026-08-22 14:40:04 +0530

    Add files via upload

--- smart_watch/rtc.h
+++ smart_watch/rtc.h
@@ -88,227 +88,115 @@ inline void syncTimeRTC() {
 
 
 
-
 //Original Example Code(TESTED and WORKING)
-
 /*
-
   Rui Santos & Sara Santos - Random Nerd Tutorials
-
   Complete project details at https://RandomNerdTutorials.com/esp32-ds1307-real-time-clock-rtc-arduino/
-
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
-
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
-
 //The clock synces with NTP every 1 hour
-
 /*
-
 #include <WiFi.h>
-
 #include <time.h>
-
 #include <Wire.h>      // Added Wire library for I2C communication
-
 #include <RTClib.h>
 
-
-
 // Define custom I2C pins for ESP32-S3
-
 #define I2C_SDA 11
-
 #define I2C_SCL 12
 
-
-
 // Enter your Wi-Fi credentials
-
 const char* ssid = "PHENIX-AX10";
-
 const char* password = "03323359065";
 
-
-
 // NTP server details
-
 const char* ntpServer = "pool.ntp.org";
-
 const long gmtOffset_sec = 0;  // Offset for GMT in seconds
-
 const int daylightOffset_sec = 19800;  // Daylight savings time in seconds
 
-
-
 // RTC object (for DS1307 or DS3231)
-
 RTC_DS1307 rtc;  // Change to RTC_DS1307 for DS1307 module
-
-
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-
-
 // Global timeinfo struct and last sync timestamp
-
 struct tm timeinfo;
-
 unsigned long lastSyncMillis = 0;  // Last sync time in milliseconds
 
-
-
 void setup() {
-
   Serial.begin(115200);
-
   setupWiFi();
 
-
-
   // Initialize I2C with custom ESP32-S3 pins
-
   Wire.begin(I2C_SDA, I2C_SCL);
 
-
-
   // Initialize RTC using the explicitly configured Wire
-
   if (!rtc.begin(&Wire)) {
-
     Serial.println("Couldn't find RTC");
-
     while (1);
-
   }
-
-
 
   // Sync the RTC at startup
-
   syncTime();
-
 }
-
-
 
 void loop() {
-
   checkTimeAndSync();  // Check if 1 hour has passed and sync if necessary
 
-
-
   // Get current time from RTC
-
   DateTime now = rtc.now();
 
-
-
   // Getting each time field in individual variables
-
   String yearStr = String(now.year(), DEC);
-
   String monthStr = (now.month() < 10 ? "0" : "") + String(now.month(), DEC);
-
   String dayStr = (now.day() < 10 ? "0" : "") + String(now.day(), DEC);
-
   String hourStr = (now.hour() < 10 ? "0" : "") + String(now.hour(), DEC);
-
   String minuteStr = (now.minute() < 10 ? "0" : "") + String(now.minute(), DEC);
-
   String secondStr = (now.second() < 10 ? "0" : "") + String(now.second(), DEC);
-
   String dayOfWeek = daysOfTheWeek[now.dayOfTheWeek()];
 
-
-
   // Complete time string
-
   String formattedTime = dayOfWeek + ", " + yearStr + "-" + monthStr + "-" + dayStr + " " + hourStr + ":" + minuteStr + ":" + secondStr;
 
-
-
   // Print the complete formatted time
-
   Serial.println(formattedTime);
 
-
-
   Serial.println();
-
   delay(10000);
-
 }
-
-
 
 void setupWiFi() {
-
     WiFi.begin(ssid, password);  // Connect to WiFi
-
     while (WiFi.status() != WL_CONNECTED) {
-
         delay(500);
-
         Serial.print(".");
-
     }
-
     Serial.println("WiFi connected.");
-
 }
-
-
 
 void syncTime() {
-
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);  // Configure time with NTP server
-
   if (!getLocalTime(&timeinfo)) {
-
     Serial.println("Failed to obtain time");
-
     return;
-
   }
-
   Serial.println("\nESP32 Time synchronized with NTP server.");
-
   Serial.print("Current time: ");
-
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 
-
-
   // Sync the RTC with the NTP time
-
   rtc.adjust(DateTime(timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday,
-
                       timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec));
 
-
-
   lastSyncMillis = millis();  // Record the last sync time in milliseconds
-
 }
-
-
 
 void checkTimeAndSync() {
-
   // Check if 1 hour has passed since the last sync (1 hour = 3600000 milliseconds)
-
   if (millis() - lastSyncMillis >= 3600000) {
-
     Serial.println("Synchronizing time with NTP...");
-
     syncTime();
-
   }
-
 }
-
 */
--- smart_watch/temperature.h
+++ smart_watch/temperature.h
@@ -45,55 +45,29 @@ inline AHT10_Data readAHT10() {
 
 
 /*  Original Example Code (TESTED and WORKING)
-
 #include <Adafruit_AHTX0.h>
-
-
 
 Adafruit_AHTX0 aht;
 
-
-
 void setup() {
-
   Serial.begin(115200);
-
   Serial.println("Adafruit AHT10 demo!");
-
-
 
   Wire.begin(11,12);
 
-
-
   if (! aht.begin()) {
-
     Serial.println("Could not find AHT10? Check wiring");
-
     while (1) delay(10);
-
   }
-
   Serial.println("AHT10 found");
-
 }
-
-
 
 void loop() {
-
   sensors_event_t humidity, temp;
-
   aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
-
   Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
-
   Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
 
-
-
   delay(500);
-
 }
-
 */
